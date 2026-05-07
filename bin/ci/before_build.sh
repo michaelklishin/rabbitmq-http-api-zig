776b2b7a1c231b1872391c81741bcdca6294461b
Michael Klishin <michaelklishin@icloud.com>
2026-05-06 18:12:09 -0700

    Set up CI on Actions

---
+++ bin/ci/before_build.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+CTL=${ZIG_HTTP_API_CLIENT_RABBITMQCTL:="sudo rabbitmqctl"}
+PLUGINS=${ZIG_HTTP_API_CLIENT_RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+
+case $CTL in
+        DOCKER*)
+          PLUGINS="docker exec ${CTL##*:} rabbitmq-plugins"
+          CTL="docker exec ${CTL##*:} rabbitmqctl";;
+esac
+
+echo "Will use rabbitmqctl at ${CTL}"
+echo "Will use rabbitmq-plugins at ${PLUGINS}"
+
+$PLUGINS enable rabbitmq_management
+
+sleep 3
+
+# guest:guest has full access to /
+
+$CTL add_vhost /
+$CTL add_user guest guest || true
+$CTL set_permissions -p / guest ".*" ".*" ".*"
+
+# Reduce retention policy for faster publishing of stats
+$CTL eval 'supervisor2:terminate_child(rabbit_mgmt_sup_sup, rabbit_mgmt_sup), application:set_env(rabbitmq_management,       sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_sup_sup:start_child().'
+$CTL eval 'supervisor2:terminate_child(rabbit_mgmt_agent_sup_sup, rabbit_mgmt_agent_sup), application:set_env(rabbitmq_management_agent, sample_retention_policies, [{global, [{605, 1}]}, {basic, [{605, 1}]}, {detailed, [{10, 1}]}]), rabbit_mgmt_agent_sup_sup:start_child().'
+
+# set cluster name
+$CTL set_cluster_name rabbitmq@localhost
+
+$CTL enable_feature_flag all
+
+# Enable plugins exercised by federation/streams/shovel tests
+$PLUGINS enable rabbitmq_shovel
+$PLUGINS enable rabbitmq_shovel_management
+
+$PLUGINS enable rabbitmq_federation
+$PLUGINS enable rabbitmq_federation_management
+
+$PLUGINS enable rabbitmq_stream
+$PLUGINS enable rabbitmq_stream_management
+
+true
